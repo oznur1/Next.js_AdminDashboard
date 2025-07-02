31f6e31fceae09869f548494bd20ad94093cdef8
oznur1 <oznuroncel10@gmail.com>
2025-07-02 11:55:27 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+import{ FC } from 'react'
+
+const Home:FC = () => {
+  return (
+    <div className='page'>
+      <h1 className='title'>Admin Paneli</h1>
+     
+    </div>
+  )
+}
+
+export default Home
+
---
+++ src/types/index.tsx
@@ -0,0 +1,15 @@
+import { JSX } from "react";
+
+
+type NavItem={
+icon:JSX.Element;
+name:string;
+url?:string;
+}
+
+
+
+
+
+
+export type{ NavItem};
